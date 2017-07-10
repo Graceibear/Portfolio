2c55d71386ac67ed27d7bb9310d0fd663834da56
Graceibear <Grace.Ouabo@students.dbcr.org>
2017-07-10 15:23:34 -0400

    Add files via upload

---
+++ Bachelorette.py
@@ -0,0 +1,33 @@
+print("My name is Grace and I am looking for my jhonny.")
+
+print("As I was walking down the street to go to GirlsWhoCode I saw a guy. I was rushing but I had a great glimpse of him. He seemed alright but he was too short for me.")
+bob = input("Type call")
+
+if bob == "call":
+    print("The next week I decided to call him. So Grace dials in his number, and waits. Bob answers, and he says that he was interested in me and he told me why he wanted to go on a date with me. However, he seemed too too stuck up.")
+    bobreply = input("Type 1 to diss him or type 2 to continue the conversation")
+    if bobreply == "2":
+        print("Bob invited me to coffee, but I hated coffee. I convinced him to pay for my dinner.")
+        print("Over chicken tenders, I looked at him in his eyes, and said 'You're proof that god has a sense of humor.' Then walked out the door.")
+        print("On my way from GirlsWhoCode, I saw a smart looking man. He was nice and tall, he looked 6'1. He was a wearing his business suit which looked like it was expensive. As I was walking he smiled at me and my heart melt. His dimples were so cute I couldn't resist. He later tap me on the shoulder and he handed me a paper that had his number on it. I was shocked because I didn't think that he noticed me. I smiled and took the paper. Later that I night, I couldn't sleep. I kept thinking about that guy. I didn't even know who I am anymore. I was freaking out about a guy that I didn't even know. The next week I decided to call him. Chickaluwe and I had a great conversation on the phone, he was kind of deep. At the end of the cal he asked me out. This is my first time going on a date so I didn't know if I should say yes or no.")
+        Chickaluwe = input("Type yay")
+
+        if Chickaluwe == "yay":
+            print("He popped the question in Paris.")
+            Chickaluwereply = input("Type yes or no to marry him ")
+            if Chickaluwereply == "yes":
+                print("Chickaluwe was with Grace for life.")
+            else:
+                print("Nah.")
+    else:
+        print("Over chicken tenders, I looked at him in his eyes, and said 'You're proof that god has a sense of humor.' Then walked out the door.")
+        print("On my way from GirlsWhoCode, I saw a smart looking man. He was nice and tall, he looked 6'1. He was a wearing his business suit which looked like it was expensive. As I was walking he smiled at me and my heart melt. His dimples were so cute I couldn't resist. He later tap me on the shoulder and he handed me a paper that had his number on it. I was shocked because I didn't think that he noticed me. I smiled and took the paper. Later that I night, I couldn't sleep. I kept thinking about that guy. I didn't even know who I am anymore. I was freaking out about a guy that I didn't even know. The next week I decided to call him. Chickaluwe and I had a great conversation on the phone, he was kind of deep. At the end of the cal he asked me out. This is my first time going on a date so I didn't know if I should say yes or no.")
+        Chickaluwe = input("Type yay")
+
+        if Chickaluwe == "yay":
+            print("He popped the question in Paris.")
+            Chickaluwereply = input("Type yes or no to marry him ")
+            if Chickaluwereply == "yes":
+                print("Chickaluwe was with Grace for life.")
+            else:
+                print("Nah.")
